d28b58bb3e9fa028733719ec3478372a7185051a
Nikolai Shishmarev <nicansh@gmail.com>
2025-05-22 23:25:37 +0300

    Homework 1.2 requests/templates some enchancements

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -5,6 +5,14 @@ from django.urls import reverse
 from django.core.paginator import Paginator
 from pagination.settings import BUS_STATION_CSV
 
+station_list = []
+with open(BUS_STATION_CSV, newline='', encoding='utf-8') as csv_file:
+    reader = csv.DictReader(csv_file)
+    for row in reader:
+        station_list.append({'Name': row['Name'],
+                             'Street': row['Street'],
+                             'District': row['District']})
+
 
 def index(request):
     return redirect(reverse('bus_stations'))
@@ -17,13 +25,7 @@ def bus_stations(request):
         page_number = int(request.GET.get('page', 1))
     except ValueError:
         page_number = 1
-    station_list = []
-    with open(BUS_STATION_CSV, newline='', encoding='utf-8') as csv_file:
-        reader = csv.DictReader(csv_file)
-        for row in reader:
-            station_list.append({'Name': row['Name'],
-                                 'Street': row['Street'],
-                                 'District': row['District']})
+
 
     paginator = Paginator(station_list, 25)
     stations = paginator.get_page(page_number)
