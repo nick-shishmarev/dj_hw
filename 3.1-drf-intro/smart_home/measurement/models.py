26e97791ef62fdb2265269a733dcb628e4003a5a
Nikolai Shishmarev <nicansh@gmail.com>
2025-06-17 22:13:35 +0300

    Homework 3.1

--- 3.1-drf-intro/smart_home/measurement/models.py
+++ 3.1-drf-intro/smart_home/measurement/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # TODO: опишите модели датчика (Sensor) и измерения (Measurement)
+
+
+class Sensor(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Measurement(models.Model):
+    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE, related_name='measurements')
+    # sensor = models.IntegerField()
+    temperature = models.DecimalField(max_digits=4, decimal_places=1)
+    measure_time = models.DateTimeField(auto_now=True)
+    # image = models.ImageField(upload_to='measurements/', null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.sensor}: {self.temperature}°C at {self.measure_time:%Y-%m-%d %T}"
--- 3.1-drf-intro/smart_home/measurement/urls.py
+++ 3.1-drf-intro/smart_home/measurement/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 
+from measurement.views import SensorUpdateView, SensorsListView, MeasurementCreateView
+
 urlpatterns = [
     # TODO: зарегистрируйте необходимые маршруты
+    path('sensors/', SensorsListView.as_view()),
+    path('sensors/<pk>/', SensorUpdateView.as_view()),
+    path('measurements/', MeasurementCreateView.as_view())
 ]
